7cc88e43ca006e16d59545c60180385787b7a1ff
hhiooiip <1497798389@qq.com>
2025-10-20 20:41:44 +0800

    Update 1_main.c

--- 1_main.c
+++ 1_main.c
@@ -1,9 +1,14 @@
-//
-//
-//
+//202511716133
+//1497798389@qq.com
+//陈渝
 #include <stdio.h>
-int main()
+
+int main() 
 {
-    printf("Hello World");
+    float c, f;
+    scanf("%f", &c);
+    f = c * 9 / 5 + 32;
+    printf("%.1f\n", f);
     return 0;
 }
+
