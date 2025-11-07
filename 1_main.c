73ed1b9c74d75d778c75e41285c955315fba8440
hhiooiip <1497798389@qq.com>
2025-11-07 17:36:38 +0800

    Update 1_main.c

--- 1_main.c
+++ 1_main.c
@@ -2,33 +2,16 @@
 //1497798389@qq.com
 //陈渝
 #include <stdio.h>
-
-int main() {
-    int n, i, f = 1;
-
-    scanf("%d", &n);
-
-    if (n <= 0 || n >= 50) {
-        printf("请输入1个小于50的正整数");
-        return 0;
+int main(){
+    int a[3][3];
+    for(int i=0;i<3;i++)
+        for(int j=0;j<3;j++)
+            scanf("%d",&a[i][j]);
+    for(int i=0;i<3;i++){
+        for(int j=0;j<3;j++)｛
+         printf("%d ",a[i][j]);
+        ｝
+        printf("\n");
     }
-
-    if (n == 1)
-        f = 0;
-    else {
-        for (i = 2; i * i <= n; i++) {
-            if (n % i == 0) {
-                f = 0;
-                break;
-            }
-        }
-    }
-
-    if (f) {
-        printf("密钥安全，密码设置成功");
-    } else {
-        printf("密钥不安全，请重新输入");
-    }
-
     return 0;
 }
