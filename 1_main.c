fc19c2fb60b777e98cb6442b1668c5f1737adc4c
hhiooiip <1497798389@qq.com>
2025-11-07 21:52:31 +0800

    Update 1_main.c

--- 1_main.c
+++ 1_main.c
@@ -8,9 +8,8 @@ int main(){
         for(int j=0;j<3;j++)
             scanf("%d",&a[i][j]);
     for(int i=0;i<3;i++){
-        for(int j=0;j<3;j++)｛
-         printf("%d ",a[i][j]);
-        ｝
+        for(int j=0;j<3;j++)
+            printf("%d ",a[i][j]);
         printf("\n");
     }
     return 0;
