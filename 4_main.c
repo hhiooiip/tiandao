42232eb72a22bb6e2e69633f16048bb81db4e2f1
hhiooiip <1497798389@qq.com>
2025-11-07 17:48:49 +0800

    Update 4_main.c

--- 4_main.c
+++ 4_main.c
@@ -2,26 +2,14 @@
 //1497798389@qq.com
 //陈渝
 #include <stdio.h>
-
-int main() {
-    int n = 100, a, b, c, f = 1;
-
-    while(n <= 999) {
-        a = (n - n % 100) / 100;
-        b = (n % 100 - n % 10) / 10;
-        c = n % 10;
-
-        if(a*a*a + b*b*b + c*c*c == n) {
-            if(f) {
-                printf("%d", n);
-                f = 0;
-            } else {
-                printf(" %d", n);
-            }
-        }
-
-        n++;
-    }
-
+int power(int a,int b){
+    int f=1;
+    for(int i=0;i<b;i++) f*=a;
+    return f;
+}
+int main(){
+    int s=0;
+    for(int i=1;i<=5;i++) s+=power(i,2);
+    printf("%d",s);
     return 0;
 }
