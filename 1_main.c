b51b533ef44a703883e41518d6880c6efbebe4fa
hhiooiip <1497798389@qq.com>
2025-11-07 21:55:31 +0800

    Update 1_main.c

--- 1_main.c
+++ 1_main.c
@@ -2,14 +2,16 @@
 //1497798389@qq.com
 //陈渝
 #include <stdio.h>
-int main(){
-    int a[3][3];
-    for(int i=0;i<3;i++)
-        for(int j=0;j<3;j++)
-            scanf("%d",&a[i][j]);
-    for(int i=0;i<3;i++){
-        for(int j=0;j<3;j++)
-            printf("%d ",a[i][j]);
+
+int main() {
+    int matrix[3][3];
+    for(int i = 0; i < 3; i++)
+        for(int j = 0; j < 3; j++)
+            scanf("%d", &matrix[i][j]);
+
+    for(int i = 0; i < 3; i++){
+        for(int j = 0; j < 3; j++)
+            printf("%d ", matrix[i][j]);
         printf("\n");
     }
     return 0;
