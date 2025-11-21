b5889419a709166b8d675852f39a5081a75871f0
hhiooiip <1497798389@qq.com>
2025-11-21 21:27:10 +0800

    更新 6_main.c

--- 6_main.c
+++ 6_main.c
@@ -2,19 +2,19 @@
 //1497798389@qq.com
 //陈渝
 #include <stdio.h>
-int sum(int a[],int n){
-    int s=0;
-    for(int i=0;i<n;i++) s+=a[i];
-    return s;
-}
-int mul(int a[],int n){
-    int m=1;
-    for(int i=0;i<n;i++) m*=a[i];
-    return m;
-}
-int main(){
-    int a[5];
-    for(int i=0;i<5;i++) scanf("%d",&a[i]);
-    printf("%d %d",sum(a,5),mul(a,5));
+#include <stdlib.h>
+
+int main() {
+    int *p = (int *)malloc(5 * sizeof(int));
+
+    for(int i = 0; i < 5; i++) {
+        scanf("%d", p + i);
+    }
+
+    for(int i = 0; i < 5; i++) {
+        printf("%d ", *(p + i));
+    }
+
+    free(p);
     return 0;
 }
