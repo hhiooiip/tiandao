3d79d56e67146aa8394091e2796928d791ca0e8d
hhiooiip <1497798389@qq.com>
2025-11-21 21:25:07 +0800

    更新 2_main.c

--- 2_main.c
+++ 2_main.c
@@ -2,18 +2,21 @@
 //1497798389@qq.com
 //陈渝
 #include <stdio.h>
-int main(){
-    int a[3][3];
-    for(int i=0;i<3;i++)｛
-        for(int j=0;j<3;j++)｛
-            scanf("%d",&a[i][j]);
-    ｝
-    ｝
-    for(int i=0;i<3;i++){
-        for(int j=0;j<3;j++)｛
-            printf("%d ",a[j][i])
-                ｝
-        printf("\n");
+
+int main() {
+    int arr[5];
+    int *p = arr;
+
+    for(int i = 0; i < 5; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    for(int i = 0; i < 5; i++) {
+        *(p + i) = *(p + i) * 2; 
+    }
+
+    for(int i = 0; i < 5; i++) {
+        printf("%d ", arr[i]);
     }
     return 0;
 }
