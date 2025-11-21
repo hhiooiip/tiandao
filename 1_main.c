f145da7f1ccccfb7b7b976a3e84fd5cdcd97ccd6
hhiooiip <1497798389@qq.com>
2025-11-21 21:24:42 +0800

    更新 1_main.c

--- 1_main.c
+++ 1_main.c
@@ -4,15 +4,14 @@
 #include <stdio.h>
 
 int main() {
-    int matrix[3][3];
-    for(int i = 0; i < 3; i++)
-        for(int j = 0; j < 3; j++)
-            scanf("%d", &matrix[i][j]);
-
-    for(int i = 0; i < 3; i++){
-        for(int j = 0; j < 3; j++)
-            printf("%d ", matrix[i][j]);
-        printf("\n");
-    }
+    int a;
+    scanf("%d", &a);
+    
+    int *p = &a;
+    *p = *p + 10;
+    
+    printf("%d,%d\n", a, *p);
     return 0;
 }
+
+    
